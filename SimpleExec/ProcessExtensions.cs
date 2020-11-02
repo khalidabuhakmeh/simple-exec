b05729b1f245554846b29c339376605e9d2b2f7e
Adam Ralph <adam@adamralph.com>
2020-11-02 09:17:34 +0100

    ProcessExtensions.RunAsync

--- SimpleExec/ProcessExtensions.cs
+++ SimpleExec/ProcessExtensions.cs
@@ -14,42 +14,88 @@ namespace SimpleExec
             process.WaitForExit();
         }
 
-        public static Task RunAsync(this Process process, bool noEcho, string echoPrefix, CancellationToken cancellationToken = default)
+        public static async Task RunAsync(this Process process, bool noEcho, string echoPrefix, CancellationToken cancellationToken = default)
         {
             var tcs = new TaskCompletionSource<object>();
-            var cancelled = false;
 
-            var registration = cancellationToken.Register(() => {
+            using (var started = new SemaphoreSlim(0, 1))
+            using (var ranToCompletionOrCanceled = new SemaphoreSlim(1, 1))
+            using (var cancellation = cancellationToken.Register(() =>
+            {
+                started.Wait();
+
+                if (tcs.Task.Status == TaskStatus.RanToCompletion)
+                {
+                    return;
+                }
+
+                ranToCompletionOrCanceled.Wait();
+
                 try
                 {
-                    cancelled = true;
-                    process.Kill();
-                }
-                catch
-                {
-                    // ignored
-                    // killing the process may timeout
-                    // and throw a different exception
-                }
-            });
+                    if (tcs.Task.Status == TaskStatus.RanToCompletion)
+                    {
+                        return;
+                    }
 
-            process.Exited += (s, e) =>
-            {
-                registration.Dispose();
-                if (cancelled)
-                {
+                    // best effort only, since exceptions may be thrown for all kinds of reasons
+                    // and the _same exception_ may be thrown for all kinds of reasons
+                    // System.Diagnostics.Process is "fine"
+                    try
+                    {
+                        process.Kill();
+                    }
+#pragma warning disable CA1031 // Do not catch general exception types
+                    catch
+#pragma warning restore CA1031 // Do not catch general exception types
+                    {
+                    }
+
+                    // SetCanceled(cancellationToken) would make more sense here
+                    // but it only exists from .NET 5 onwards
+                    // see https://github.com/dotnet/runtime/issues/30862
+                    // however, we know TrySetCanceled will succeed
+                    // because we have full control of the underlying task
                     tcs.TrySetCanceled(cancellationToken);
                 }
-                else
+                finally
                 {
-                    tcs.SetResult(default);
+                    ranToCompletionOrCanceled.Release();
                 }
-            };
+            }))
+            {
+                process.Exited += (s, e) =>
+                {
+                    cancellation.Dispose();
 
-            process.EnableRaisingEvents = true;
-            process.EchoAndStart(noEcho, echoPrefix);
+                    if (tcs.Task.Status == TaskStatus.Canceled)
+                    {
+                        return;
+                    }
 
-            return tcs.Task;
+                    ranToCompletionOrCanceled.Wait();
+
+                    try
+                    {
+                        if (tcs.Task.Status == TaskStatus.Canceled)
+                        {
+                            return;
+                        }
+
+                        tcs.SetResult(default);
+                    }
+                    finally
+                    {
+                        ranToCompletionOrCanceled.Release();
+                    }
+                };
+
+                process.EnableRaisingEvents = true;
+                process.EchoAndStart(noEcho, echoPrefix);
+                started.Release();
+
+                await tcs.Task.ConfigureAwait(false);
+            }
         }
 
         private static void EchoAndStart(this Process process, bool noEcho, string echoPrefix)
